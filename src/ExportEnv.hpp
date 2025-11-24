956a2405d51815dc444323a31b69bfe3a97b9267
prime-run <prime0run@gmail.com>
2025-11-23 17:28:25 -0700

    nuke old cod base

--- src/ExportEnv.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ConfigUtils.hpp"
-
-#include <vector>
-
-namespace hyprquery {
-void exportEnv(const std::vector<QueryResult> &results,
-               const std::vector<QueryInput> &queries);
-}
--- src/ExportJson.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "ConfigUtils.hpp"
-#include <vector>
-
-namespace hyprquery {
-void exportJson(const std::vector<QueryResult> &results);
-}
